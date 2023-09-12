71a8e6eb8791a2b42cd99e1febc53107cb21e300
QuincyForbes <quincyforbes@pm.me>
2023-09-12 00:18:14 -0700

    Comments

--- app/controller/business.py
+++ app/controller/business.py
@@ -1,7 +1,6 @@
 from typing import Optional, Dict, List, Union
 from app.model import kindle_model
 
-
 # Global allowed keys for searching
 ALLOWED_KEYS = [
     "pages",
@@ -28,7 +27,14 @@ class ValidationError(Exception):
 
 
 def validate_target(target: Optional[str]) -> None:
-    """Validate that the target is either None or one of the allowed keys."""
+    """
+    Validate that the target is either None or one of the allowed keys.
+
+    Args:
+        target (Optional[str]): The target key to validate.
+    Raises:
+        ValidationError: If the target is not None and not in ALLOWED_KEYS.
+    """
     if target is not None and target not in ALLOWED_KEYS:
         raise ValidationError(
             f"Invalid target. Allowed keys for searching are {', '.join(ALLOWED_KEYS)}."
@@ -36,6 +42,15 @@ def validate_target(target: Optional[str]) -> None:
 
 
 def list_books(library_path: str) -> List[Dict[str, Union[str, int]]]:
+    """
+    List books in a library.
+
+    Args:
+        library_path (str): The path to the library.
+
+    Returns:
+        List[Dict[str, Union[str, int]]]: A list of book dictionaries.
+    """
     library_instance = kindle_model.Library(library_path)
     return library_instance.list_books()
 
@@ -43,6 +58,20 @@ def list_books(library_path: str) -> List[Dict[str, Union[str, int]]]:
 def find_book(
     key: str, value: str, library_path: str, target: Optional[str] = None
 ) -> List[Dict[str, Union[str, int]]]:
+    """
+    Find books in a library based on a key-value pair.
+
+    Args:
+        key (str): The key to search for (e.g., "title", "author").
+        value (str): The value to search for.
+        library_path (str): The path to the library.
+        target (Optional[str]): The target key to retrieve from matching books.
+
+    Returns:
+        List[Dict[str, Union[str, int]]]: A list of matching book dictionaries.
+    Raises:
+        BookNotFoundError: If no matching books are found.
+    """
     validate_target(target)
     library_instance = kindle_model.Library(library_path)
     found = library_instance.find_books(**{key: value})
@@ -56,11 +85,26 @@ def find_book(
 def add_book_user(
     book_uuid: str, global_library_path: str, user_library_path: str
 ) -> Dict[str, Union[str, Dict[str, Union[str, int]]]]:
+    """
+    Add a book to a user's library.
+
+    Args:
+        book_uuid (str): The UUID of the book to add.
+        global_library_path (str): The path to the global library.
+        user_library_path (str): The path to the user's library.
+
+    Returns:
+        Dict[str, Union[str, Dict[str, Union[str, int]]]]: A dictionary indicating the status
+        and the added book.
+    Raises:
+        ValidationError: If the book already exists in the user's library.
+        BookNotFoundError: If the book is not found in the global library.
+    """
     user_library_instance = kindle_model.Library(user_library_path)
     global_library_instance = kindle_model.Library(global_library_path)
     found_user = user_library_instance.find_books(uuid=book_uuid)
     if found_user:
-        raise ValidationError("Book already exists in user's library.")
+        raise ValidationError("Book already exists in the user's library.")
     found_global = global_library_instance.find_books(uuid=book_uuid)
     if not found_global:
         raise BookNotFoundError("Book not found in the global library.")
@@ -72,6 +116,17 @@ def add_book_user(
 def add_book_global(
     data: Dict[str, Union[str, int]], global_library_path: str
 ) -> Dict[str, Union[str, Dict[str, Union[str, int]]]]:
+    """
+    Add a book to the global library.
+
+    Args:
+        data (Dict[str, Union[str, int]]): The book data.
+        global_library_path (str): The path to the global library.
+
+    Returns:
+        Dict[str, Union[str, Dict[str, Union[str, int]]]]: A dictionary indicating the status
+        and the added book.
+    """
     global_library_instance = kindle_model.Library(global_library_path)
     new_book = kindle_model.Book(**data)
     global_library_instance.add_book(new_book)
@@ -81,6 +136,18 @@ def add_book_global(
 def subtract_book_user(
     book_uuid: str, user_library_path: str
 ) -> Dict[str, Union[str, int]]:
+    """
+    Remove a book from a user's library.
+
+    Args:
+        book_uuid (str): The UUID of the book to remove.
+        user_library_path (str): The path to the user's library.
+
+    Returns:
+        Dict[str, Union[str, int]]: A dictionary indicating the status and the removed book UUID.
+    Raises:
+        BookNotFoundError: If the book is not found in the user's library.
+    """
     user_library_instance = kindle_model.Library(user_library_path)
     found_user = user_library_instance.find_books(uuid=book_uuid)
     if not found_user:
@@ -92,11 +159,24 @@ def subtract_book_user(
 def find_top_book_user(
     user_library_path: str, target: Optional[str] = None
 ) -> Dict[str, Union[str, Dict[str, Union[str, int]]]]:
+    """
+    Find the top book in a user's library based on a target key.
+
+    Args:
+        user_library_path (str): The path to the user's library.
+        target (Optional[str]): The target key to use for finding the top book.
+
+    Returns:
+        Dict[str, Union[str, Dict[str, Union[str, int]]]]: A dictionary indicating the status
+        and the top book based on the target key.
+    Raises:
+        BookNotFoundError: If there are no books in the user's library.
+    """
     validate_target(target)
     user_library_instance = kindle_model.Library(user_library_path)
     books_user = user_library_instance.list_books()
     if not books_user:
-        raise BookNotFoundError("No books in user's library.")
+        raise BookNotFoundError("No books in the user's library.")
     top_book = max(books_user, key=lambda book: book.get(target, float("-inf")))
     return {"status": "success", f"highest_value: {target}": top_book}
 
@@ -104,6 +184,21 @@ def find_top_book_user(
 def change_book_page_user(
     book_uuid: str, page_number: str, user_library_path: str
 ) -> Dict[str, str]:
+    """
+    Change the page number of a book in a user's library.
+
+    Args:
+        book_uuid (str): The UUID of the book to update.
+        page_number (str): The new page number.
+        user_library_path (str): The path to the user's library.
+
+    Returns:
+        Dict[str, str]: A dictionary indicating the status and the updated book UUID.
+    Raises:
+        ValidationError: If the page number is not an integer.
+        BookNotFoundError: If the book is not found in the user's library.
+        ValidationError: If the page number exceeds the total pages of the book.
+    """
     try:
         page_number = int(page_number)
     except ValueError:
@@ -112,7 +207,7 @@ def change_book_page_user(
     found_books = user_library_instance.find_books(uuid=book_uuid)
     if not found_books:
         raise BookNotFoundError(
-            "No book with the specified UUID exists in user's library."
+            "No book with the specified UUID exists in the user's library."
         )
     total_pages = found_books[0].get("pages", None)
     if total_pages is not None and page_number > total_pages:
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -25,17 +25,39 @@ def register_routes(app: Any) -> None:
 def format_response(
     data: Union[List[Dict[str, Any]], Dict[str, Any]], status: int = 200
 ) -> Any:
+    """
+    Format the API response as JSON with a status code.
+
+    Args:
+        data (Union[List[Dict[str, Any]], Dict[str, Any]]): Data to include in the response.
+        status (int): HTTP status code.
+
+    Returns:
+        Any: JSON response with data and status code.
+    """
     return jsonify({"data": data, "status": status})
 
 
 @book_routes.route("/user/books", methods=["GET"])
 def get_all_books_user() -> Any:
+    """
+    Get a list of all books in the user's library.
+
+    Returns:
+        Any: JSON response with the list of books.
+    """
     books = list_books(user_json)
     return format_response(books)
 
 
 @book_routes.route("/global/books", methods=["GET"])
 def get_all_books_global() -> Any:
+    """
+    Get a list of all books in the global library.
+
+    Returns:
+        Any: JSON response with the list of books.
+    """
     books = list_books(global_json)
     return format_response(books)
 
@@ -45,6 +67,17 @@ def get_all_books_global() -> Any:
 )
 @book_routes.route("/global/books/search/<key>/<value>/<target>", methods=["GET"])
 def search_book_global(key: str, value: str, target: str = None) -> Any:
+    """
+    Search for books in the global library based on a key-value pair.
+
+    Args:
+        key (str): The key to search for (e.g., "title", "author").
+        value (str): The value to search for.
+        target (str, optional): The target key to retrieve from matching books.
+
+    Returns:
+        Any: JSON response with the list of matching books.
+    """
     books = find_book(key, value, global_json, target=target)
     return format_response(books)
 
@@ -54,6 +87,17 @@ def search_book_global(key: str, value: str, target: str = None) -> Any:
 )
 @book_routes.route("/user/books/search/<key>/<value>/<target>", methods=["GET"])
 def search_book_user(key: str, value: str, target: str = None) -> Any:
+    """
+    Search for books in the user's library based on a key-value pair.
+
+    Args:
+        key (str): The key to search for (e.g., "title", "author").
+        value (str): The value to search for.
+        target (str, optional): The target key to retrieve from matching books.
+
+    Returns:
+        Any: JSON response with the list of matching books.
+    """
     try:
         books = find_book(key, value, user_json, target=target)
         return format_response(books)
@@ -63,6 +107,15 @@ def search_book_user(key: str, value: str, target: str = None) -> Any:
 
 @book_routes.route("/user/books/<uuid>", methods=["POST"])
 def add_book_to_user_library(uuid: str) -> Any:
+    """
+    Add a book to the user's library.
+
+    Args:
+        uuid (str): The UUID of the book to add.
+
+    Returns:
+        Any: JSON response with the status and the added book details.
+    """
     try:
         response = add_book_user(uuid, global_json, user_json)
         return format_response(response)
@@ -72,6 +125,12 @@ def add_book_to_user_library(uuid: str) -> Any:
 
 @book_routes.route("/global/books", methods=["POST"])
 def add_book_to_global_library() -> Any:
+    """
+    Add a book to the global library.
+
+    Returns:
+        Any: JSON response with the status and the added book details.
+    """
     data: Dict[str, Union[str, int]] = request.get_json()
     response = add_book_global(data, global_json)
     return format_response(response)
@@ -79,24 +138,58 @@ def add_book_to_global_library() -> Any:
 
 @book_routes.route("/user/books/<uuid>", methods=["DELETE"])
 def remove_book_from_user_library(uuid: str) -> Any:
+    """
+    Remove a book from the user's library.
+
+    Args:
+        uuid (str): The UUID of the book to remove.
+
+    Returns:
+        Any: JSON response with the status and the removed book UUID.
+    """
     response = subtract_book_user(uuid, user_json)
     return format_response(response)
 
 
 @book_routes.route("/user/books/top/<target>", methods=["GET"])
 def get_top_user_book(target: str = None) -> Any:
+    """
+    Get the top book in the user's library based on a target key.
+
+    Args:
+        target (str, optional): The target key to use for finding the top book.
+
+    Returns:
+        Any: JSON response with the status and the top book based on the target key.
+    """
     book = find_top_book_user(user_json, target=target)
     return format_response(book)
 
 
 @book_routes.route("/user/books/last-read", methods=["GET"])
 def get_last_user_book() -> Any:
+    """
+    Get the last-read book in the user's library.
+
+    Returns:
+        Any: JSON response with the status and the last-read book.
+    """
     book = find_top_book_user(user_json, "last_read_date")
     return format_response(book)
 
 
 @book_routes.route("/user/books/<uuid>/page/<page_number>", methods=["PATCH"])
 def update_book_page_for_user(uuid: str, page_number: str) -> Any:
+    """
+    Update the page number of a book in the user's library.
+
+    Args:
+        uuid (str): The UUID of the book to update.
+        page_number (str): The new page number.
+
+    Returns:
+        Any: JSON response with the status and the updated book UUID.
+    """
     try:
         page_number = int(page_number)
     except ValueError:
@@ -110,5 +203,5 @@ def update_book_page_for_user(uuid: str, page_number: str) -> Any:
     except BookNotFoundError as bne:
         return {
             "status": "error",
-            "message": "No book with the specified UUID exists in user's library.",
+            "message": "No book with the specified UUID exists in the user's library.",
         }, 404
